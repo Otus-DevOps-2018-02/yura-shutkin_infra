6aca737dd3fe37a93295b25a16f71a4062b08967
Shutkin Yura <yshutkin@syntheticode.net>
2018-03-28 01:24:00 +0500

    Startup script updated. Since now should be launched as appuser user
    Signed-off-by: Shutkin Yura <yshutkin@syntheticode.net>

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+sudo -u appuser bash - <<EOF
+cd /home/appuser
 git clone -b cloud-testapp  https://github.com/Otus-DevOps-2018-02/yura-shutkin_infra.git automaton && \
 cd automaton && \
 ./install_ruby.sh && \
 ./install_mongodb.sh && \
 ./deploy.sh
+EOF
