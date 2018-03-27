03472c14b31a010341075ba0e888dcc894447e2b
Shutkin Yura <yshutkin@syntheticode.net>
2018-03-28 01:34:23 +0500

    Fixed app base path. Application started at /home/appuser/automaton/reddit instead of /home/appuser/reddit
    Signed-off-by: Shutkin Yura <yshutkin@syntheticode.net>

--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@
 sudo -u appuser bash - <<EOF
 cd /home/appuser
 git clone -b cloud-testapp  https://github.com/Otus-DevOps-2018-02/yura-shutkin_infra.git automaton && \
-cd automaton && \
-./install_ruby.sh && \
-./install_mongodb.sh && \
-./deploy.sh
+./automaton/install_ruby.sh && \
+./automaton/install_mongodb.sh && \
+./automaton/deploy.sh
+rm -Rfv automaton
 EOF
